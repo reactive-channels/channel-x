df3407d2378b2e2e2581e5d654c5ae07906b6e83
talitman <talitmandotan@gmail.com>
2023-01-06 14:47:36 +0200

    fixed Publishers

--- packages/devtools.channel/src/lib/internal/modules/scopes/index.ts
+++ packages/devtools.channel/src/lib/internal/modules/scopes/index.ts
@@ -1,5 +1,5 @@
 import Channels from "./channels/Channels.vue";
 import Components from "./components/Components.vue";
 import Consumers from "./consumers/Consumers.vue";
-import publishers from "./publishers/Publishers.vue";
-export default { Channels, Components, Consumers, publishers };
+import Publishers from "./publishers/Publishers.vue";
+export default { Channels, Components, Consumers, Publishers };
